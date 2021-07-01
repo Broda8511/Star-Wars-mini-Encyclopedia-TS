6c5d6f64f9de9842c44dbedb77a2819b384a69fb
Adam Megger <adammegger@int.pl>
2021-07-01 19:54:16 +0200

    Update VehicleDetails.tsx

--- src/pages_details/VehicleDetails.tsx
+++ src/pages_details/VehicleDetails.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const VehicleDetails = () => {
-  console.log();
   return (
     <div className="vehicles-details">
       <div>
