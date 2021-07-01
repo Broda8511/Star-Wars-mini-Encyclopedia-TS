8df7d577a2c63560424436f092e32a3e5f5866a0
Adam Megger <adammegger@int.pl>
2021-07-01 19:50:07 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -18,15 +18,3 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import {BrowserRouter as Router} from 'react-router-dom';
-// import './index.css';
-// import App from './App';
-
-// ReactDOM.render(
-//     <Router>
-//       <App />
-//     </Router>,
-//   document.getElementById('root')
-// );
