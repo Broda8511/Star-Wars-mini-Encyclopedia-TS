728296da1feb1ddfee8918430d1db665459dd905
Adam Megger <adammegger@int.pl>
2021-07-01 19:54:00 +0200

    Update StarshipDetails.tsx

--- src/pages_details/StarshipDetails.tsx
+++ src/pages_details/StarshipDetails.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const StarshipDetails = () => {
-  console.log();
   return (
     <div className="starships-details">
       <div>
